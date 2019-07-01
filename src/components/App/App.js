9187d3a5bcaace57402d7efd59df9f6f27f7a178
caio.almeida@digitalrepublic.com.br <Caio Almeida>
2019-07-01 14:21:09 -0300

    Final style app settings.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Card } from '../index'
 import Payment from 'payment'
+import './index.scss'
 
 
 export default class App extends Component {
@@ -49,21 +50,32 @@ export default class App extends Component {
     if(event.target.value.length > 0) {
       this.setState({ ...this.state, rotate: true, cvv: event.target.value })
     } else {
-      this.setState({ ...this.state, cvv: '' })
+      this.setState({ ...this.state, cvv: '', rotate: false })
+    }
+  }
+
+  onHandleNumber = e => {
+    const charCode = (e.which) ? e.which : e.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)){
+      return false;
+    }
+    else{
+       return true;
     }
   }
 
   render() {
 
-    const { type, number, name, cvv, date } = this.state
+    const { type, number, name, cvv, date, rotate } = this.state
   
     return (    
-      <div>
-        <input type="text" onChange={this.onHandleChangeNumber} maxLength="16" placeholder="cardNumber" />
-        <input type="text" onChange={this.onHandleChangeName} placeholder="nameCard" />
-        <input type="tel" onChange={this.onHandleChangeCVV} placeholder="CVV" maxLength="4" />
-        
-        <Card type={type} numberCard={number} validThru={date} nameCard={name} cvvCard={cvv} />
+      <div className="container">
+        <div className="form-credit-card">
+          <input type="number" onChange={this.onHandleChangeNumber} placeholder="NUMBER CARD" />
+          <input type="text" onChange={this.onHandleChangeName} placeholder="YOUR NAME" />
+          <input type="number" onChange={this.onHandleChangeCVV} placeholder="CVV" />
+        </div>
+        <Card type={type} numberCard={number} rotateCard={rotate} validThru={date} nameCard={name} cvvCard={cvv} />
       </div>
     )
   }
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,18 +2,31 @@ import React from 'react'
 import './index.scss'
 
 export default function Card(props) {
-    const { type, numberCard, nameCard } = props
+    console.log(props)
+    const { type, numberCard, nameCard, rotateCard, cvvCard } = props
     return (
-        <div className="ccr-content">
-            <div className={`ccr-content__type ${type ? `ccr-content__type--${type}` : '' }`} />
-            <div className="ccr-content__body">
-                <span className="ccr-content__chip"></span>
-                <div className="ccr-content__numberCard"> { numberCard } </div>
-                <div className="ccr-content__nameCard">
-                    <label>YOUR NAME</label>
-                    <p>{ nameCard } </p>
+        <>
+        <div className={`ccr-content ${rotateCard ? `ccr-content__is-flipped` : '' }`}>
+            <div className="ccr-content__face ccr-content__face--front">
+                <div className={`ccr-content__type ${type ? `ccr-content__type--${type}` : '' }`} />
+                <div className="ccr-content__body">
+                    <span className="ccr-content__chip"></span>
+                    <div className="ccr-content__numberCard"> { numberCard } </div>
+                    <div className="ccr-content__nameCard">
+                        <label>YOUR NAME</label>
+                        <p>{ nameCard } </p>
+                    </div>
+                </div>
+            </div>
+        
+            <div className="ccr-content__face ccr-content__face--back">
+                <div className="ccr-content__stripe"/>
+                <div className="ccr-content__cvvCard">
+                    <label>CVV</label>
+                    <p>{ cvvCard }</p>
                 </div>
             </div>
         </div>
+        </>
     )
 }
